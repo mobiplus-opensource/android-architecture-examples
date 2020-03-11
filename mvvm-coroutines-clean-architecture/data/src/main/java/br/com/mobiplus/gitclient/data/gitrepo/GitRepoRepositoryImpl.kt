0f7a06ca9cb2321f8e7c5c46e4371ea03a2eef0f
Luis Fernandez <luis82fernandez@gmail.com>
2020-03-10 23:03:25 -0300

    Implementing example of error object transform

--- mvvm-coroutines-clean-architecture/data/src/main/java/br/com/mobiplus/gitclient/data/gitrepo/GitRepoRepositoryImpl.kt
+++ mvvm-coroutines-clean-architecture/data/src/main/java/br/com/mobiplus/gitclient/data/gitrepo/GitRepoRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package br.com.mobiplus.gitclient.data.gitrepo
 
 import br.com.mobiplus.gitclient.data.gitrepo.remote.GitRepoRemoteDataSource
+import br.com.mobiplus.gitclient.data.gitrepo.remote.model.GetRepoListResponse
 import br.com.mobiplus.gitclient.domain.base.BaseErrorData
 import br.com.mobiplus.gitclient.domain.base.resultwrapper.ResultWrapper
 import br.com.mobiplus.gitclient.domain.model.GitRepoModel
@@ -17,10 +18,26 @@ class GitRepoRepositoryImpl(
     ): ResultWrapper<List<GitRepoModel>, BaseErrorData<GithubError>> {
         val remoteResult = gitRepoRemoteDataSource.getGitRepoList(page, language)
 
-        return remoteResult.transformSuccess { getReposResponse ->
+        return remoteResult.transform(
+            this.handleGetGitRepoListSuccess(),
+            this.handleGetGitRepoListError()
+        )
+    }
+
+    private fun handleGetGitRepoListSuccess(): (GetRepoListResponse) -> List<GitRepoModel> {
+        return { getReposResponse ->
             getReposResponse.gitRepoList.map { repoPayload ->
                 repoPayload.mapTo()
             }
+        }
+    }
+
+    private fun handleGetGitRepoListError(): (BaseErrorData<GithubError>) -> BaseErrorData<GithubError> {
+        return { baseErrorData ->
+            BaseErrorData(
+                errorBody = baseErrorData.errorBody,
+                errorMessage = "API ERROR: ${baseErrorData.errorMessage}"
+            )
         }
     }
 

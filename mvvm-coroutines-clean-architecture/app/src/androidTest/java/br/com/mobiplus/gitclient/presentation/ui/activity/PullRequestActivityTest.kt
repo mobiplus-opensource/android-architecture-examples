b2397f39895b1bc88bbc1c2bbd33e7129a307d3f
Luis Fernandez <luis82fernandez@gmail.com>
2020-03-24 10:49:02 -0300

    Implementing UI end to end test (mocking Data Source response)

--- mvvm-coroutines-clean-architecture/app/src/androidTest/java/br/com/mobiplus/gitclient/presentation/ui/activity/PullRequestActivityTest.kt
+++ mvvm-coroutines-clean-architecture/app/src/androidTest/java/br/com/mobiplus/gitclient/presentation/ui/activity/PullRequestActivityTest.kt
@@ -4,14 +4,14 @@ import android.content.Intent
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.rule.ActivityTestRule
 import br.com.mobiplus.gitclient.R
-import br.com.mobiplus.gitclient.domain.FakePullRequestModel
+import br.com.mobiplus.gitclient.data.pullRequest.PullRequestAPIDataSource
+import br.com.mobiplus.gitclient.data.pullRequest.PullRequestRepositoryImpl
+import br.com.mobiplus.gitclient.data.pullRequest.model.PullRequestResponseModel
+import br.com.mobiplus.gitclient.data.pullRequest.model.UserResponseModel
 import br.com.mobiplus.gitclient.domain.FakeResultWrapper
-import br.com.mobiplus.gitclient.domain.FakeUserModel
-import br.com.mobiplus.gitclient.domain.usecases.GetPullRequestUseCase
+import br.com.mobiplus.gitclient.domain.repository.PullRequestRepository
 import br.com.mobiplus.gitclient.presentation.ui.pullRequest.details.PullRequestActivity
-import br.com.mobiplus.gitclient.presentation.ui.pullRequest.details.PullRequestViewModel
 import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
-import com.schibsted.spain.barista.interaction.BaristaSleepInteractions.sleep
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
 import io.mockk.impl.annotations.MockK
@@ -19,9 +19,9 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.loadKoinModules
 import org.koin.dsl.module
+import java.util.*
 
 @RunWith(AndroidJUnit4ClassRunner::class)
 class PullRequestActivityTest {
@@ -30,7 +30,7 @@ class PullRequestActivityTest {
     val rule = ActivityTestRule(PullRequestActivity::class.java, false, false)
 
     @MockK
-    private lateinit var getPullRequestUseCase: GetPullRequestUseCase
+    private lateinit var pullRequestAPIDataSource: PullRequestAPIDataSource
 
     @Before
     fun before() {
@@ -38,9 +38,9 @@ class PullRequestActivityTest {
         MockKAnnotations.init(this)
 
         loadKoinModules(module {
-            viewModel(override = true) {
-                PullRequestViewModel(
-                    getPullRequestUseCase
+            factory<PullRequestRepository>(override = true) {
+                PullRequestRepositoryImpl(
+                        pullRequestAPIDataSource
                 )
             }
         })
@@ -49,10 +49,23 @@ class PullRequestActivityTest {
     @Test
     fun openActivity() {
         coEvery {
-            getPullRequestUseCase.runAsync(any())
-        } returns FakeResultWrapper.mockSuccess(
-            FakePullRequestModel.mock(
-                FakeUserModel.mock()
+            pullRequestAPIDataSource.getPullRequest(any(), any(), any())
+        } returns FakeResultWrapper.mockFullSuccess(
+            success = PullRequestResponseModel(
+                    number = 1,
+                    htmlUrl = "",
+                    title = "#FAKE_TITLE",
+                    userModel = UserResponseModel(
+                            login = "luis.fernandez",
+                            avatarUrl = "http://avatar.url"
+                    ),
+                    body = "",
+                    createdAt = Date(),
+                    comments = 8,
+                    commits = 4,
+                    additions = 56,
+                    deletions = 45,
+                    changedFiles = 23
             )
         )
 
@@ -63,8 +76,12 @@ class PullRequestActivityTest {
 
         rule.launchActivity(intent)
 
-        sleep(300)
-
         assertDisplayed(R.id.textAuthorName, "luis.fernandez")
+        assertDisplayed(R.id.textTitle, "#FAKE_TITLE")
+        assertDisplayed(R.id.textComments, "Comments: 8")
+        assertDisplayed(R.id.textCommits, "Commits: 4")
+        assertDisplayed(R.id.textAdditions, "Additions: 56")
+        assertDisplayed(R.id.textDeletions, "Deletions: 45")
+        assertDisplayed(R.id.textChangedFiles, "Changed Files: 23")
     }
 }
